d6edd5a4d97000eb57faa7784de4ed9f895dbf44
Ali Hashim <ahashim@u.rochester.edu>
2017-11-29 15:48:40 -0500

    StreetMap java can read file

--- src/StreetMap.java
+++ src/StreetMap.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class StreetMap {
 
 	public static void main(String[] args) {
-		System.out.print(args[0]);
+		String input = args[0];
+		BufferedReader br = null;
+		try {
+	        String currentLine;
+	        br = new BufferedReader(new FileReader(input));
+	        while ((currentLine = br.readLine()) != null) {
+	               String[] strArr = currentLine.split("\t");
+	               System.out.println(strArr[0]);
+	        }
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                if (br != null) {
+	                	br.close();
+	                }
+	            } catch (IOException ex) {
+	                ex.printStackTrace();
+	            }
+	        }
+
 	}
 
 }
