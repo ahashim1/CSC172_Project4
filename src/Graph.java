bd4bd7812856df959e4c52c2179687ea992232c9
Ali Hashim <ahashim@u.rochester.edu>
2017-11-29 23:42:01 -0500

    Added Insert Edges

--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,31 @@
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph {
+	static ArrayList<Edge> adjacentEdges = new ArrayList<Edge>();
+	static int numVertices;
+	static int numEdges;
+	
+	public Graph() {
+		numVertices = 0;
+		numEdges = 0;
+	}
+	
+	static void insertEdge(Edge e) {
+		numEdges += 1;
+		adjacentEdges.add(e);
+	}
     
+	static boolean removeEdge(Edge e) {
+		return false;
+	}
+	
+	static void insertVertex(Vertex v) {
+	}
+    
+	static boolean removeVertex(Vertex v) {
+		return false;
+	}
+	
+	
+	
 }
